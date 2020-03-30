639909c789e4366b08f58772e0ed0817c478b8d3
Nico Tripcevich <tripcevich@berkeley.edu>
2020-03-30 00:02:18 -0700

    fix url

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1P4dS92NZO0phVgzPqh9w5tukIJ2hU2VvHFx5wiiHUnI/edit#gid=164271551';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1P4dS92NZO0phVgzPqh9w5tukIJ2hU2VvHFx5wiiHUnI/edit#gid=0';
